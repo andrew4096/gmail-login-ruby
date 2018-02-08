9e024e5dd0e01b487b74de5badaf0160ba6fb3e0
AndrewLakhno <andrew.l@tenengroup.com>
2018-02-08 14:17:33 +0300

    Update comments

--- desktop/g_login_tc.rb
+++ desktop/g_login_tc.rb
@@ -15,7 +15,7 @@ require './helpers/spec_steps.rb'
     end
 
     it "TC#1 - Gmail customer login" , :story => "Login with correct data", :severity => :normal do |e|
-      e.step "Check proper page" do |s|
+      e.step "Validate test setup: Check proper page" do |s|
         visit_loginpage
       end
 
@@ -24,31 +24,17 @@ require './helpers/spec_steps.rb'
       end
 
       e.step "Enter customer password" do |s|
-        set_password("Pa$$w0rd`")
+        set_password("Pa$$w0rd")
       end
         
-      e.step "Check mailbox - login success" do |s|
+      e.step "Check mailbox: Login success" do |s|
         check_mailbox
         expect(page).to have_content("Andrew Lakhno")
       end
 
-      e.step "Cleanup" do |s|
+      e.step "Cleanup: Cleanup session and coocies" do |s|
         cleanup
       end  
   end
-
-  # describe "Something else ",
-  #  :feature => "AS a user I'm ...ing",
-  #   js: true, :order => :defined do
-    
-  #     before(:step) do |s|
-  #       puts "Before step #{s.current_step}"
-  #     end
-  
-  #     before (:each) do
-  #       visit main_site
-  #     end
-  
-  # end
 
 end
