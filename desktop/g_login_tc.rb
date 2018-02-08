7b0687dc4a6d0f1ab7f78af3fdeab05c7a8075fc
AndrewLakhno <andrew.l@tenengroup.com>
2018-02-08 14:36:20 +0300

    Fix typo

--- desktop/g_login_tc.rb
+++ desktop/g_login_tc.rb
@@ -24,7 +24,7 @@ require './helpers/spec_steps.rb'
       end
 
       e.step "Enter customer password" do |s|
-        set_password("Pa$$w0rd`")
+        set_password("Pa$$w0rd")
       end
         
       e.step "Check mailbox: Login success" do |s|
